aa53fdd96e1263e90b3c4620d4ce223a8b9b9f30
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-11-04 16:20:11 -0400

    Changed indent settings

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.smarttab = true
-vim.opt.smartindent = true
+vim.opt.cindent = true
 vim.opt.wrap = true
 vim.opt.guicursor = "i:block"
 vim.cmd("set path+=**")
