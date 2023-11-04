b024aeb0cf7460db7be00a5130a566d8397480cf
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-11-04 19:50:46 -0400

    Changed Inlay Hints Default

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,9 +1,19 @@
+-- Misc Options
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Indent Settings
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.smarttab = true
 vim.opt.cindent = true
 vim.opt.wrap = true
+
+-- Change Cursor to Block During Insert Mode
 vim.opt.guicursor = "i:block"
+
+-- Disable Inlay Hints by Default 
+vim.g.hints_enabled = false
+
+-- Add Recursive Search to Path
 vim.cmd("set path+=**")
--- lua/user/plugins/clang-extensions.lua
+++ lua/user/plugins/clang-extensions.lua
@@ -1,19 +1,29 @@
 local lsp_nmap = require("user.utils").lsp_nmap
 
+local function set_hints_accordingly()
+  if vim.g.hints_enabled then
+    require("clangd_extensions.inlay_hints").set_inlay_hints()
+  else
+    require("clangd_extensions.inlay_hints").disable_inlay_hints()
+  end
+end
+
 return {
-    "p00f/clangd_extensions.nvim",
-    opts = {},
-    config = function()
-        lvim.lsp.on_attach_callback = function(client, _)
-            if client.name == "clangd" then
-                require("clangd_extensions.inlay_hints").setup_autocmd()
-                require("clangd_extensions.inlay_hints").set_inlay_hints()
-                lsp_nmap("<leader>lh", { function()
-                    require("clangd_extensions.inlay_hints").toggle_inlay_hints()
-                end, "Toggle Inlay Hints" })
-                lsp_nmap("<leader>lH", { "<cmd>ClangdSwitchSourceHeader<cr>", "Switch Between Header and Source" })
-                lsp_nmap("<leader>lI", {"<cmd>ClangdSymbolInfo<cr>", "Clangd Symbol Info"})
-            end
-        end
+  "p00f/clangd_extensions.nvim",
+  opts = {},
+  config = function()
+    lvim.lsp.on_attach_callback = function(client, _)
+      if client.name == "clangd" then
+        require("clangd_extensions.inlay_hints").setup_autocmd()
+        set_hints_accordingly()
+
+        lsp_nmap("<leader>lh", { function()
+          vim.g.hints_enabled = not vim.g.hints_enabled
+          set_hints_accordingly()
+        end, "Toggle Inlay Hints" })
+        lsp_nmap("<leader>lH", { "<cmd>ClangdSwitchSourceHeader<cr>", "Switch Between Header and Source" })
+        lsp_nmap("<leader>lI", { "<cmd>ClangdSymbolInfo<cr>", "Clangd Symbol Info" })
+      end
     end
+  end
 }
